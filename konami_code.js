8b1a46a020758d2d66f0dfebc3e500134ec8369f
Ethan Collins <ethantcollins98@gmail.com>
2019-04-18 18:34:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,17 +12,5 @@ const codes = [
 ];
 
 function init() {
-  const main = document.getElementById('main');
-    let index = 0;
-  main.addEventListener('key', onKeyDownHandler(e)) {
-    const key = e.key;
-
-    if (key === codes[index]) {
-      index++;
-      if (index === codes.length) {
-        main.alert("Hurray!");
-        index = 0;
-      }
-    }
-  }
+  
 }
