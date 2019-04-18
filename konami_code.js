e06ddc12b653fa2e0cf938c88710ced25a4b2fc3
Ethan Collins <ethantcollins98@gmail.com>
2019-04-18 18:49:30 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,7 +13,7 @@ const codes = [
 
 function init() {
   const body = document.body
-  body.addEventListener('keydown', function(key)) {
+  body.addEventListener('keydown', function(e)) {
     console.log(e.key)
   }
   }
